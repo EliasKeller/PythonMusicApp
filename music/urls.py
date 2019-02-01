c49174b7a06cddc7be9d29d43c202605f6bb6beb
Elias Keller <elias.keller@gmail.com>
2019-02-01 18:14:51 +0100

    Feat: Added "add album" redirect doesnt work yet -> 31--5:00

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     path('', views.IndexView.as_view(), name='index'),
     path('<pk>/', views.DetailView.as_view(), name='detail'),
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
 
 ]
