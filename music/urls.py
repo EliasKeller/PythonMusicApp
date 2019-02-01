a1944062dfe9b161d62d92a1c63fb0e92786f0ce
Elias Keller <elias.keller@gmail.com>
2019-02-01 15:59:18 +0100

    Feat: Edit Views to Generic

--- music/urls.py
+++ music/urls.py
@@ -3,12 +3,8 @@ from . import views
 
 app_name = 'music'
 urlpatterns = [
-    # /music/
-    path('', views.index, name='index'),
 
-    # /music/<album_id>/
-    path('<int:album_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<pk>/', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album_id>/favorite/
-    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
