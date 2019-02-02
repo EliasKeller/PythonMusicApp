ffa475dbaf8a109bb3ccd651c397bf7c28b8eb92
Elias Keller <elias.kelle@gmail.com>
2019-02-02 16:12:43 +0100

    Feat: Add album done; Delete Album WIP; Update Album WIP

--- music/urls.py
+++ music/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<pk>/', views.DetailView.as_view(), name='detail'),
     path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+    path('album/<pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+    path('album/<pk>/delete', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
